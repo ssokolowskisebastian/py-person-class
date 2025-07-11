968f2962cf5858318efea3b9180edd4b40b34e9d
Sebastian Sokolowski <wielewiak08@gmail.com>
2025-07-11 14:39:30 +0200

    add people dict

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 class Person:
+    people = {"name": None, "person": None}
+
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-
+        Person.people.update({"name": self.name, "person": self})
 
 
 def create_person_list(people: list) -> list:
